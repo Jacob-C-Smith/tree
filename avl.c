39e5ba328df8a58b037bbbf93e0732e3e9faf1d2
Jacob Smith <jacobsmithsinbox@gmail.com>
2023-12-11 01:30:12 -0700

    AVL stub

---
+++ avl.c
@@ -0,0 +1,9 @@
+/** !
+ * Implementation of Adelson-Velsky and Landis tree
+ * 
+ * @file avl.c
+ * 
+ * @author Jacob Smith
+ */
+
+#include <tree/avl.h>
---
+++ include/tree/avl.h
@@ -0,0 +1,19 @@
+/** !
+ * Include header for Adelson-Velsky and Landis tree
+
+ * @file tree/avl.h 
+ * 
+ * @author Jacob Smith
+ */
+
+// Include guard
+#pragma once
+
+// Standard library
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+// sync submodule
+#include <sync/sync.h>
