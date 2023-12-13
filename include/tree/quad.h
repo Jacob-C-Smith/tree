96c9863329d031fb162ec2ca94cc47a5ec1cabe7
Jacob Smith <jacobsmithsinbox@gmail.com>
2023-12-12 22:56:26 -0700

    Quadtree stub

---
+++ include/tree/quad.h
@@ -0,0 +1,19 @@
+/** !
+ * Include header for quadtree
+ * 
+ * @file tree/quad.h 
+ * 
+ * @author Jacob Smith
+ */
+
+// Include guard
+#pragma once
+
+// Standard library
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+// sync submodule
+#include <sync/sync.h>
---
+++ quad.c
@@ -0,0 +1,9 @@
+/** !
+ * Implementation of quad tree
+ * 
+ * @file quad.c
+ * 
+ * @author Jacob Smith
+ */
+
+#include <tree/binary.h>
