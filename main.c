2ad9fc8ff518cbfd6e25df52e81dc48ee4780423
Jacob Smith <jacobsmithsinbox@gmail.com>
2024-07-22 15:14:47 -0600

    [example] Improved example

--- main.c
+++ main.c
@@ -117,17 +117,6 @@ int tree_r_example ( int argc, const char *argv[] );
 int tree_red_black_example ( int argc, const char *argv[] );
 
 /** !
- * Convert text to two bit values. 
- * A -> 00, C -> 01, G-> 10, and T -> 11.
- * 
- * @param p_text   the text
- * @param p_result return
- * 
- * @return 1 on success, 0 on error
- */
-int ascii_to_u64_encoded_2_bit_slice ( const char *const p_text, unsigned long long *p_result );
-
-/** !
  * Example binary tree node serializer
  * 
  * @param p_file             the file to write to
@@ -146,6 +135,27 @@ int binary_tree_example_serializer ( FILE *p_file, binary_tree_node *p_binary_tr
  * @return 1 on success, 0 on error
 */
 int binary_tree_example_parser ( FILE *p_file, binary_tree_node *p_binary_tree_node );
+
+/** !
+ * Print a binary tree node to standard out
+ * 
+ * @param p_key   the key
+ * @param p_value the value
+ * 
+ * @return 1 on success, 0 on error
+ */
+int binary_tree_print_node ( void *p_key, void *p_value );
+
+/** !
+ * Convert text to two bit values. 
+ * A -> 00, C -> 01, G-> 10, and T -> 11.
+ * 
+ * @param p_text   the text
+ * @param p_result return
+ * 
+ * @return 1 on success, 0 on error
+ */
+int ascii_to_u64_encoded_2_bit_slice ( const char *const p_text, unsigned long long *p_result );
 
 /** !
  * Return the size of a file IF buffer == 0 ELSE read a file into buffer
@@ -175,10 +185,10 @@ int main ( int argc, const char *argv[] )
     srand(time(0));
 
     // Formatting
-    log_info(
-        "╭──────────────╮\n"\
-        "│ tree example │\n"\
-        "╰──────────────╯\n"\
+    log_info("╭──────────────╮\n");
+    log_info("│ tree example │\n");
+    log_info("╰──────────────╯\n");
+    printf(
         "The tree library provides high level abstractions for %d different trees.\n"\
         "The AVL tree, the B tree, the binary tree, the quadtree, the R tree, and the red black tree.\n\n"\
         "An AVL tree is self balancing binary search tree; suitable for frequent searching.\n"\
@@ -204,15 +214,13 @@ int main ( int argc, const char *argv[] )
     if ( examples_to_run[TREE_EXAMPLE_B] )
 
         // Error check
-        ;//if ( tree_b_example(argc, argv) == 0 ) goto failed_to_run_b_tree_example;
-        
+        if ( tree_b_example(argc, argv) == 0 ) goto failed_to_run_b_tree_example;
         
     // Run the binary tree example program
     if ( examples_to_run[TREE_EXAMPLE_BINARY] )
 
         // Error check
         if ( tree_binary_example(argc, argv) == 0 ) goto failed_to_run_binary_tree_example;
-        
 
     // Run the quadtree example program
     if ( examples_to_run[TREE_EXAMPLE_QUAD] )
@@ -220,13 +228,11 @@ int main ( int argc, const char *argv[] )
         // Error check
         if ( tree_quad_example(argc, argv) == 0 ) goto failed_to_run_quad_tree_example;
         
-
     // Run the R tree example program
     if ( examples_to_run[TREE_EXAMPLE_R] )
 
         // Error check
         if ( tree_r_example(argc, argv) == 0 ) goto failed_to_run_r_tree_example;
-        
 
     // Run the red black tree example program
     if ( examples_to_run[TREE_EXAMPLE_RED_BLACK] )
@@ -234,7 +240,6 @@ int main ( int argc, const char *argv[] )
         // Error check
         if ( tree_red_black_example(argc, argv) == 0 ) goto failed_to_run_red_black_tree_example;
         
-
     // Success
     return EXIT_SUCCESS;
 
@@ -384,12 +389,10 @@ int tree_avl_example ( int argc, const char *argv[] )
 {
 
     // Formatting
-    log_info(
-        "╭──────────────────╮\n"\
-        "│ AVL tree example │\n"\
-        "╰──────────────────╯\n"\
-        "This example TODO: Describe example.\n\n"
-    );
+    log_info("╭──────────────────╮\n");
+    log_info("│ AVL tree example │\n");
+    log_info("╰──────────────────╯\n");
+    printf("This example TODO: Describe example.\n\n");
 
     // Success
     return 1;
@@ -399,21 +402,19 @@ int tree_b_example ( int argc, const char *argv[] )
 {
 
     // Formatting
-    log_info(
-        "╭────────────────╮\n"\
-        "│ B tree example │\n"\
-        "╰────────────────╯\n"\
-        "This example creates a B tree from the DNA of E. Coli\n\n"
-    );
+    log_info("╭────────────────╮\n");
+    log_info("│ B tree example │\n");
+    log_info("╰────────────────╯\n");
+    printf("This example creates a B tree from the DNA of E. Coli\n\n");
     
     // Initialized data
     b_tree *p_b_tree = { 0 };
 
     // Construct a B tree
-    if ( b_tree_construct(&p_b_tree, "output.b_tree", 0, 3, 64) == 0 ) goto failed_to_create_b_tree;
+    //if ( b_tree_construct(&p_b_tree, "output.b_tree", 0, 3, 64) == 0 ) goto failed_to_create_b_tree;
 
     // Add data to the B tree
-    goto add_e_coli_genome;
+    //goto add_e_coli_genome;
 
     // Done
     done_adding_genome:
@@ -499,10 +500,10 @@ int tree_binary_example ( int argc, const char *argv[] )
     (void) argv;
 
     // Formatting
-    log_info(
-        "╭─────────────────────╮\n"\
-        "│ Binary tree example │\n"\
-        "╰─────────────────────╯\n"\
+    log_info("╭─────────────────────╮\n");
+    log_info("│ Binary tree example │\n");
+    log_info("╰─────────────────────╯\n");
+    printf(
         "This example creates a balanced binary tree from a sorted list of keys\n"\
         "in linear time. This balanced binary tree is serialized to the disk,\n"
         "loaded, and a key is searched for at random.\n\n"
@@ -520,13 +521,13 @@ int tree_binary_example ( int argc, const char *argv[] )
     unsigned long long keys [BINARY_TREE_EXAMPLE_LIST_LENGTH] = { 8,4,12,2,6,10,14,1,3,5,7,9,11,13,15 };
 
     // Log
-    printf("Constructing tree...\n");
+    printf("Constructing tree... ");
 
     // Construct a tree
     if ( binary_tree_construct(&p_binary_tree, 0, 16) == 0 ) goto failed_to_construct_tree;
 
     // Log
-    printf("Inserting properties...\n");
+    printf("DONE\nInserting properties... ");
 
     // Iterate over each property
     for (size_t i = 0; i < 15; i++)
@@ -535,7 +536,13 @@ int tree_binary_example ( int argc, const char *argv[] )
         (void) binary_tree_insert(p_binary_tree, (const void *const) keys[i], (const void *const) values[i]);
 
     // Log
-    printf("Serializing tree...\n");
+    printf("DONE\nPrinting tree...\n\n");
+    
+    // Traverse the binary tree using the in order technique
+    binary_tree_traverse_inorder(p_binary_tree, (binary_tree_traverse_fn *)binary_tree_print_node);
+
+    // Log
+    printf("\nDONE\nSerializing tree... ");
 
     // Serialize the binary tree to a file
     if ( binary_tree_serialize(p_binary_tree, "../output.binary_tree", binary_tree_example_serializer) == 0 ) goto failed_to_serialize_binary_tree;
@@ -544,7 +551,7 @@ int tree_binary_example ( int argc, const char *argv[] )
     if ( binary_tree_destroy(&p_binary_tree) == 0 ) goto failed_to_destroy_binary_tree;
 
     // Log
-    printf("Parsing tree...\n");
+    printf("DONE\nParsing tree... ");
 
     // Load the binary tree from the file
     if ( binary_tree_parse(&p_binary_tree, "../output.binary_tree", 0, (binary_tree_parse_fn *) binary_tree_example_parser) == 0 ) goto failed_to_parse_binary_tree;
@@ -556,7 +563,7 @@ int tree_binary_example ( int argc, const char *argv[] )
     if ( binary_tree_search(p_binary_tree, (void *) random_index, &p_result) == 0 ) goto failed_to_search_binary_tree;
 
     // Print the random index
-    printf("Searching \"%d\" yields \"%s\"\n\n", random_index, p_result);
+    printf("DONE\n\nSearching \"%d\" yields \"%s\"\n\n", random_index, p_result);
 
     // Success
     return 1;
@@ -613,12 +620,10 @@ int tree_quad_example ( int argc, const char *argv[] )
 {
 
     // Formatting
-    log_info(
-        "╭──────────────────╮\n"\
-        "│ Quadtree example │\n"\
-        "╰──────────────────╯\n"\
-        "This example simulates particles in a 2D plane.\n\n"
-    );
+    log_info("╭──────────────────╮\n");
+    log_info("│ Quadtree example │\n");
+    log_info("╰──────────────────╯\n");
+    printf("This example simulates particles in a 2D plane.\n\n");
     
     // Success
     return 1;
@@ -628,12 +633,10 @@ int tree_r_example ( int argc, const char *argv[] )
 {
 
     // Formatting
-    log_info(
-        "╭────────────────╮\n"\
-        "│ R tree example │\n"\
-        "╰────────────────╯\n"\
-        "This example TODO: Describe example.\n\n"
-    );
+    log_info("╭────────────────╮\n");
+    log_info("│ R tree example │\n");
+    log_info("╰────────────────╯\n");
+    printf("This example TODO: Describe example.\n\n");
     
     // Success
     return 1;
@@ -643,12 +646,10 @@ int tree_red_black_example ( int argc, const char *argv[] )
 {
 
     // Formatting
-    log_info(
-        "╭────────────────────────╮\n"\
-        "│ Red black tree example │\n"\
-        "╰────────────────────────╯\n"\
-        "This example TODO: Describe example.\n\n"
-    );
+    log_info("╭────────────────────────╮\n");
+    log_info("│ Red black tree example │\n");
+    log_info("╰────────────────────────╯\n");
+    printf("This example TODO: Describe example.\n\n");
     
     // Success
     return 1;
@@ -773,6 +774,16 @@ int binary_tree_example_parser ( FILE *p_file, binary_tree_node *p_binary_tree_n
 
     // Store the value
     strncpy(p_binary_tree_node->p_value, &_value, 8);
+
+    // Success
+    return 1;
+}
+
+int binary_tree_print_node ( void *p_key, void *p_value )
+{
+
+    // Print the property
+    log_info("%-2d : %s\n", p_key, p_value);
 
     // Success
     return 1;
